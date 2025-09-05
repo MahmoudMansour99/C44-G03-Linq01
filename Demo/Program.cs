de5ba82d3e72494d7b1b8c6d3c0863a03de7d881
Mahmoud Mansour <mansour@gmail.com>
2025-09-05 19:18:59 +0300

    Complete Part 01 Implictly Type Local Variable [var - dynamic]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Implictly Type Local Variable [var - dynamic]
+
+            #region Var Keyword
+            //string Data = "Ahmed";
+            //var Data = "Ahmed"; // Implictly Type 
+            //                    // Compiler will detect Datatype of variable at compilation time
+            //                    // Based on its inital value 
+
+            ////var x = null; // invalid 
+
+            //Data = null;
+            //var Data; // Inavlid 
+            //Data = "Mustafa";
+
+            //Data = 10; // Invalid 
+            #endregion
+
+            #region dynamic keyword
+            //dynamic Data = "Ali";
+            // CLR Will detect Datatype of variable at runtime
+            // based on its last assigned value
+
+            //dynamic X;
+
+            //dynamic Y = 10;
+
+            //Data = 10;
+
+            //Console.WriteLine(Data);
+            //Data = true;
+            //Console.WriteLine(Data);
+
+            #endregion
+
+            #endregion
+
+
         }
     }
 }
